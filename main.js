25a638f4cb1b954f7da890b7980b3143fbfc1db0
karimgamal22 <113974075+karimgamal22@users.noreply.github.com>
2022-09-26 23:32:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+let title = document.querySelector(".title");
+let turn = "x";
+let squares = [];
+
+function end(num1, num2, num3) {
+  title.innerHTML = `${squares[num1]} winner`;
+  document.getElementById("itme" + num1).style.background = "#000";
+  document.getElementById("itme" + num2).style.background = "#000";
+  document.getElementById("itme" + num3).style.background = "#000";
+  setInterval(function () {
+    title.innerHTML += ".";
+  }, 1000);
+  setTimeout(function () {
+    location.reload();
+  }, 4000);
+}
+
+function winner() {
+  for (let i = 1; i < 10; i++) {
+    squares[i] = document.getElementById("itme" + i).innerHTML;
+  }
+  if (
+    squares[1] == squares[2] &&
+    squares[2] == squares[3] &&
+    squares[1] != ""
+  ) {
+    end(1, 2, 3);
+  } else if (
+    squares[4] == squares[5] &&
+    squares[5] == squares[6] &&
+    squares[4] != ""
+  ) {
+    end(4, 5, 6);
+  } else if (
+    squares[7] == squares[8] &&
+    squares[8] == squares[9] &&
+    squares[7] != ""
+  ) {
+    end(7, 8, 9);
+  }
+  // end العرض
+  else if (
+    squares[1] == squares[4] &&
+    squares[4] == squares[7] &&
+    squares[4] != ""
+  ) {
+    end(1, 4, 7);
+  } else if (
+    squares[2] == squares[5] &&
+    squares[5] == squares[8] &&
+    squares[5] != ""
+  ) {
+    end(2, 5, 8);
+  } else if (
+    squares[9] == squares[3] &&
+    squares[3] == squares[6] &&
+    squares[3] != ""
+  ) {
+    end(3, 6, 9);
+  }
+  // end الطول
+  else if (
+    squares[1] == squares[5] &&
+    squares[5] == squares[9] &&
+    squares[9] != ""
+  ) {
+    end(1, 5, 9);
+  } else if (
+    squares[3] == squares[5] &&
+    squares[5] == squares[7] &&
+    squares[7] != ""
+  ) {
+    end(3, 5, 7);
+  }
+}
+
+function games(id) {
+  let div = document.getElementById(id);
+  if (turn === "x" && div.innerHTML == "") {
+    div.innerHTML = "x";
+    turn = "o";
+    title.innerHTML = "o";
+  } else if (turn === "o" && div.innerHTML == "") {
+    div.innerHTML = "o";
+    turn = "x";
+    title.innerHTML = "x";
+  }
+  winner();
+}
+
+/*
+
+*/
